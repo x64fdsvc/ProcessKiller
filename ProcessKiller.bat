da4e033d37a67b5ae85701a21569166c0bdae0da
x64fdsvc <vltzbiggestfan@gmail.com>
2024-06-24 23:37:24 +1000

    Update ProcessKiller.bat
    updated to version 1.1

--- ProcessKiller.bat
+++ ProcessKiller.bat
@@ -1,15 +1,18 @@
-rem A Simple ProcessKiller Made by @ztlv_, June 2024.
-rem $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-rem //////////////////////////////////////////////////
+rem ///A Simple ProcessKiller Made by @ztlv_, June 2024.///
+rem $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+rem ///////////////////////////////////////////////////////
 
 color 0d
-mode con:cols=120 lines=30
+mode con:cols=77  lines=40
 @Title ProcessKiller - %DATE% - %TIME% 
 @ECHO OFF
 set ldt=%date% %time%
 ECHO %ldt%>> ProcessKillerLogs.txt / Ran ProcessKiller on Chrome.exe
-TASKLIST  |   FINDSTR  chrome.exe   ||  START chrome.exe
-@ECHO "Kill > Chrome.exe"
+ECHO FINDSTR LOCATING PID's . . .
+ECHO [Process Name]               [PID] [Session name]    [Session#]        [MEM]
+TASKLIST  |   FINDSTR  chrome.exe   ||  START chrome.exe  
+ECHO FINDSTR FINISHED.
+@ECHO Kill Process?
 @ECHO OFF
 PAUSE
 setlocal EnableExtensions EnableDelayedExpansion
@@ -18,7 +21,7 @@ set "UserChoice=!UserChoice: =!"
 if /I not "!UserChoice!" == "Y" endlocal & exit /B
 cls
 color 0c
-taskkill /IM chrome.exe /F
+taskkill /IM chrome.exe /F 
 @ECHO [ProcessKiller] will now terminate
 @ECHO OFF
 PAUSE 
