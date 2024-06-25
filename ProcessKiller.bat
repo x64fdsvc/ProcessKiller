e37bbe86a871243a27d7149498f6e281d37820c8
x64fdsvc <vltzbiggestfan@gmail.com>
2024-06-25 13:21:34 +1000

    Update ProcessKiller.bat

--- ProcessKiller.bat
+++ ProcessKiller.bat
@@ -1,16 +1,12 @@
-rem /////////////////////////////////////////////////////
-rem ///// https://github.com/x64fdsvc/ProcessKiller ////
-rem ///////////////////////////////////////////////////
-
 color 0d
-mode con:cols=77  lines=40
+mode con:cols=77  lines=30
 @Title ProcessKiller - %DATE% - %TIME% 
 @ECHO OFF
 set ldt=%date% %time%
-@ECHO %ldt%>> ProcessKillerLogs.txt / Ran ProcessKiller on Chrome.exe
+@ECHO %ldt%>> ProcessKillerLogs.txt / Ran ProcessKiller on Discord.exe
 @ECHO [FINDSTR] Running tasklist . . .
 @ECHO [Process Name]               [PID] [Session name]    [Session#]        [MEM]
-TASKLIST  |   FINDSTR /I  chrome.exe   ||  START /I chrome.exe  
+TASKLIST  |   FINDSTR /I  discord.exe   ||  START /I discord.exe  
 ECHO [FINDSTR] Finished.
 @ECHO Run taskkill?
 @ECHO OFF
@@ -21,10 +17,8 @@ set "UserChoice=!UserChoice: =!"
 if /I not "!UserChoice!" == "Y" endlocal & exit /B
 cls
 color 0c
-taskkill /IM chrome.exe /F 
+taskkill /IM discord.exe /F 
 @ECHO [ProcessKiller] Will now terminate
 @ECHO OFF
 PAUSE 
 EXIT /B 
-
-rem //////////////////////////////////////////////////
